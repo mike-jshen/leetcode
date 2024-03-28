cfaaf22853627803a9fde372862e876bc7596048
mike-jshen <mike.shen@outlook.com>
2024-03-28 11:21:09 -0400

    longest substring

---
+++ longest-substring.py
@@ -0,0 +1,17 @@
+
+def longestSub(self, s: str) -> int:
+    l = 0
+    cur = {} # store character position in hash
+    result = 0
+    for c , v in enumerate(s):
+
+        if v in cur:
+            l = max(l,cur[v] + 1)
+
+        cur[v] = c
+        result = max(result , c - l)
+    return result + 1
+    
+if __name__ == '__main__':
+    string = "teststring"
+    print(longestSub("test"))
