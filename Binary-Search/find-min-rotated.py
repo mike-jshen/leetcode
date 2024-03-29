535797476cb08faae30972a80add48b300086d14
mike-jshen <68671792+mike-jshen@users.noreply.github.com>
2024-03-29 11:07:43 -0400

    Create find-min-rotated.py

---
+++ Binary-Search/find-min-rotated.py
@@ -0,0 +1,23 @@
+class Solution:
+  def findMin(self, nums: List[int]) -> int:
+    if len(nums) == 1:
+      return nums[0]
+
+    l = 0
+    r = len(nums) - 1
+    mid = math.floor((l+r)/2)
+    while l < r:
+        if nums[mid] > nums[r]:
+            print("increase mid")
+            l = mid + 1
+            mid = math.floor((r+l)/2)
+
+        elif nums[mid] < nums[l]:
+            print("decrease mid")
+            r = mid
+            mid = math.floor((r+l)/2)
+            
+        else:
+            return nums[l]
+
+    return nums[mid]
